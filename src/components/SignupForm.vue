3e91ec6d0550471f567cd8c260712d10d489f613
Atticus Yong <86176575+AtticusYong@users.noreply.github.com>
2021-12-15 01:06:24 +0000

    Initial commit

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,13 +1,37 @@
 <template>
   <form>
     <label>Email: </label>
-    <input type="email" required>
+    <input type="email" required v-model="email">
+
+    <label>Password: </label>
+    <input type="password" required v-model="password">
+
+    <label>Role: </label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <div class="terms">
+      <input type="checkbox" v-model="" required>
+      <label>Accept terms and conditions</label>
+    </div>
+
   </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        email: 'theoddoneout64@gmail.com',
+        password: '37377373',
+        role: 'designer'
+      }
+    }
   }
 </script>
 
@@ -29,7 +53,7 @@
     letter-spacing: 1px;
     font-weight: bold;
   }
-  input {
+  input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;
@@ -37,5 +61,12 @@
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+  }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
   }
 </style>
